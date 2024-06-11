1c387a3ad8ddf0bf3d038adee995e58a90e9aabb
liminjun <liminjun@cmss.chinamobile.com>
2024-06-11 21:37:42 +0800

    feat:天气API改用高德地图

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,4 @@
-// Copyright 2016 Google Inc.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//      http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
-(function() {
+(function () {
   'use strict';
 
   var app = {
@@ -24,7 +9,7 @@
     cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('.main'),
     addDialog: document.querySelector('.dialog-container'),
-    daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    daysOfWeek: ['一', '二', '三', '四', '五', '六', '日']
   };
 
 
@@ -34,17 +19,17 @@
    *
    ****************************************************************************/
 
-  document.getElementById('butRefresh').addEventListener('click', function() {
+  document.getElementById('butRefresh').addEventListener('click', function () {
     // Refresh all of the forecasts
     app.updateForecasts();
   });
 
-  document.getElementById('butAdd').addEventListener('click', function() {
+  document.getElementById('butAdd').addEventListener('click', function () {
     // Open/show the add new city dialog
     app.toggleAddDialog(true);
   });
-
-  document.getElementById('butAddCity').addEventListener('click', function() {
+  // 添加城市按钮
+  document.getElementById('butAddCity').addEventListener('click', function () {
     // Add the newly selected city
     var select = document.getElementById('selectCityToAdd');
     var selected = select.options[select.selectedIndex];
@@ -54,12 +39,12 @@
       app.selectedCities = [];
     }
     app.getForecast(key, label);
-    app.selectedCities.push({key: key, label: label});
+    app.selectedCities.push({ key: key, label: label });
     app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
-  document.getElementById('butAddCancel').addEventListener('click', function() {
+  document.getElementById('butAddCancel').addEventListener('click', function () {
     // Close the add new city dialog
     app.toggleAddDialog(false);
   });
@@ -72,7 +57,7 @@
    ****************************************************************************/
 
   // Toggles the visibility of the add new city dialog.
-  app.toggleAddDialog = function(visible) {
+  app.toggleAddDialog = function (visible) {
     if (visible) {
       app.addDialog.classList.add('dialog-container--visible');
     } else {
@@ -80,24 +65,63 @@
     }
   };
 
+  app.getNextForecast = function (card, cityCode) {
+    // 未来4天天气
+    var url = 'https://restapi.amap.com/v3/weather/weatherInfo?key=8fe20bc651c9b48f7b2be936de344043&extensions=all&city=' + cityCode;
+    // Fetch the latest data.
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          var forecastData = response.forecasts[0].casts;
+
+          var nextDays = card.querySelectorAll('.future .oneday');
+          var today = new Date();
+          today = today.getDay();
+          // 0,1,2,3
+          for (var i = 0; i < 4; i++) {
+            var nextDay = nextDays[i];
+            var daily = forecastData[i];
+            if (daily && nextDay) {
+              nextDay.querySelector('.date').textContent =
+                app.daysOfWeek[(i + today) % 7];
+              nextDay.querySelector('.icon').classList.add(app.getIconClass(daily.week));
+              nextDay.querySelector('.temp-high .value').textContent =
+                Math.round(daily.daytemp_float);
+              nextDay.querySelector('.temp-low .value').textContent =
+                Math.round(daily.nighttemp_float);
+            }
+          }
+
+
+
+        }
+      } else {
+        // Return the initial weather forecast since no data is available.
+        // app.updateForecastCard(initialWeatherForecast);
+      }
+    };
+    request.open('GET', url);
+    request.send();
+
+
+  }
   // Updates a weather card with the latest weather forecast. If the card
   // doesn't already exist, it's cloned from the template.
-  app.updateForecastCard = function(data) {
-    var dataLastUpdated = new Date(data.created);
-    var sunrise = data.channel.astronomy.sunrise;
-    var sunset = data.channel.astronomy.sunset;
-    var current = data.channel.item.condition;
-    var humidity = data.channel.atmosphere.humidity;
-    var wind = data.channel.wind;
+  app.updateForecastCard = function (data) {
+    var current = data[0];
+    var dataLastUpdated = new Date(current.reporttime);
 
-    var card = app.visibleCards[data.key];
+
+    var card = app.visibleCards[current.adcode];
     if (!card) {
       card = app.cardTemplate.cloneNode(true);
       card.classList.remove('cardTemplate');
-      card.querySelector('.location').textContent = data.label;
+      card.querySelector('.location').textContent = current.city;
       card.removeAttribute('hidden');
       app.container.appendChild(card);
-      app.visibleCards[data.key] = card;
+      app.visibleCards[current.adcode] = card;
     }
 
     // Verifies the data provide is newer than what's already visible
@@ -112,36 +136,20 @@
         return;
       }
     }
-    cardLastUpdatedElem.textContent = data.created;
-
-    card.querySelector('.description').textContent = current.text;
-    card.querySelector('.date').textContent = current.date;
-    card.querySelector('.current .icon').classList.add(app.getIconClass(current.code));
+    cardLastUpdatedElem.textContent = current.reporttime;
+    card.querySelector('.description').textContent = current.city;
+    card.querySelector('.date').textContent = current.reporttime;
+    // card.querySelector('.current .icon').classList.add(app.getIconClass(current));
     card.querySelector('.current .temperature .value').textContent =
-      Math.round(current.temp);
-    card.querySelector('.current .sunrise').textContent = sunrise;
-    card.querySelector('.current .sunset').textContent = sunset;
+      Math.round(current.temperature);
+
     card.querySelector('.current .humidity').textContent =
-      Math.round(humidity) + '%';
-    card.querySelector('.current .wind .value').textContent =
-      Math.round(wind.speed);
-    card.querySelector('.current .wind .direction').textContent = wind.direction;
-    var nextDays = card.querySelectorAll('.future .oneday');
-    var today = new Date();
-    today = today.getDay();
-    for (var i = 0; i < 7; i++) {
-      var nextDay = nextDays[i];
-      var daily = data.channel.item.forecast[i];
-      if (daily && nextDay) {
-        nextDay.querySelector('.date').textContent =
-          app.daysOfWeek[(i + today) % 7];
-        nextDay.querySelector('.icon').classList.add(app.getIconClass(daily.code));
-        nextDay.querySelector('.temp-high .value').textContent =
-          Math.round(daily.high);
-        nextDay.querySelector('.temp-low .value').textContent =
-          Math.round(daily.low);
-      }
-    }
+      Math.round(current.humidity) + '%';
+    card.querySelector('.current .wind .value').textContent = current.windpower;
+    card.querySelector('.current .wind .direction').textContent = current.winddirection;
+
+    app.getNextForecast(card, current.adcode);
+
     if (app.isLoading) {
       app.spinner.setAttribute('hidden', true);
       app.container.removeAttribute('hidden');
@@ -164,44 +172,40 @@
    * request goes through, then the card gets updated a second time with the
    * freshest data.
    */
-  app.getForecast = function(key, label) {
-    var statement = 'select * from weather.forecast where woeid=' + key;
-    var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
-        statement;
+  app.getForecast = function (key, label) {
+    var url = 'https://restapi.amap.com/v3/weather/weatherInfo?key=8fe20bc651c9b48f7b2be936de344043&city=' + key;
     // TODO add cache logic here
-    if ('caches' in window) {
-      /*
-       * Check if the service worker has already cached this city's weather
-       * data. If the service worker has the data, then display the cached
-       * data while the app fetches the latest data.
-       */
-      caches.match(url).then(function(response) {
-        if (response) {
-          response.json().then(function updateFromCache(json) {
-            var results = json.query.results;
-            results.key = key;
-            results.label = label;
-            results.created = json.query.created;
-            app.updateForecastCard(results);
-          });
-        }
-      });
-    }
+    // if ('caches' in window) {
+    //   /*
+    //    * Check if the service worker has already cached this city's weather
+    //    * data. If the service worker has the data, then display the cached
+    //    * data while the app fetches the latest data.
+    //    */
+    //   caches.match(url).then(function(response) {
+    //     if (response) {
+    //       response.json().then(function updateFromCache(json) {
+    //         debugger
+    //         var results = json.lives;
+    //         results.adcode = key;
+    //         results.city = label;
+    //         results.created = json.lives.reporttime;
+    //         app.updateForecastCard(results);
+    //       });
+    //     }
+    //   });
+    // }
     // Fetch the latest data.
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+    request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var response = JSON.parse(request.response);
-          var results = response.query.results;
-          results.key = key;
-          results.label = label;
-          results.created = response.query.created;
+          var results = response.lives;
           app.updateForecastCard(results);
         }
       } else {
         // Return the initial weather forecast since no data is available.
-        app.updateForecastCard(initialWeatherForecast);
+        // app.updateForecastCard(initialWeatherForecast);
       }
     };
     request.open('GET', url);
@@ -209,21 +213,21 @@
   };
 
   // Iterate all of the cards and attempt to get the latest forecast data
-  app.updateForecasts = function() {
+  app.updateForecasts = function () {
     var keys = Object.keys(app.visibleCards);
-    keys.forEach(function(key) {
+    keys.forEach(function (key) {
       app.getForecast(key);
     });
   };
 
   // TODO add saveSelectedCities function here
   // Save list of cities to localStorage.
-  app.saveSelectedCities = function() {
+  app.saveSelectedCities = function () {
     var selectedCities = JSON.stringify(app.selectedCities);
     localStorage.selectedCities = selectedCities;
   };
 
-  app.getIconClass = function(weatherCode) {
+  app.getIconClass = function (weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
     weatherCode = parseInt(weatherCode);
     switch (weatherCode) {
@@ -292,41 +296,19 @@
    * or when the user has not saved any cities. See startup code for more
    * discussion.
    */
-  var initialWeatherForecast = {
-    key: '2459115',
-    label: 'New York, NY',
-    created: '2016-07-22T01:00:00Z',
-    channel: {
-      astronomy: {
-        sunrise: "5:43 am",
-        sunset: "8:21 pm"
-      },
-      item: {
-        condition: {
-          text: "Windy",
-          date: "Thu, 21 Jul 2016 09:00 PM EDT",
-          temp: 56,
-          code: 24
-        },
-        forecast: [
-          {code: 44, high: 86, low: 70},
-          {code: 44, high: 94, low: 73},
-          {code: 4, high: 95, low: 78},
-          {code: 24, high: 75, low: 89},
-          {code: 24, high: 89, low: 77},
-          {code: 44, high: 92, low: 79},
-          {code: 44, high: 89, low: 77}
-        ]
-      },
-      atmosphere: {
-        humidity: 56
-      },
-      wind: {
-        speed: 25,
-        direction: 195
-      }
-    }
-  };
+  var initialWeatherForecast = [{
+    "province": "江苏",
+    "city": "苏州市",
+    "adcode": "320500",
+    "weather": "阴",
+    "temperature": "23",
+    "winddirection": "东",
+    "windpower": "≤3",
+    "humidity": "70",
+    "reporttime": "2024-06-10 19:02:14",
+    "temperature_float": "23.0",
+    "humidity_float": "70.0"
+  }];
   // TODO uncomment line below to test app with fake data
   app.updateForecastCard(initialWeatherForecast);
 
@@ -345,7 +327,7 @@
   app.selectedCities = localStorage.selectedCities;
   if (app.selectedCities) {
     app.selectedCities = JSON.parse(app.selectedCities);
-    app.selectedCities.forEach(function(city) {
+    app.selectedCities.forEach(function (city) {
       app.getForecast(city.key, city.label);
     });
   } else {
@@ -356,15 +338,15 @@
      */
     app.updateForecastCard(initialWeatherForecast);
     app.selectedCities = [
-      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+      { key: initialWeatherForecast[0].adcode, label: initialWeatherForecast[0].city }
     ];
     app.saveSelectedCities();
   }
 
   // TODO add service worker code here
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker
+  //            .register('./service-worker.js')
+  //            .then(function() { console.log('Service Worker Registered'); });
+  // }
 })();
